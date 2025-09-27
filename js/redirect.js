bed48c947dba928c135cbbac340e19011faac5a0
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-09-27 17:34:59 -0300

    Redirect to login

---
+++ js/redirect.js
@@ -0,0 +1,37 @@
+const LOGIN_PAGE_URL = './login.html'; 
+
+function getUser() {
+
+    const userString = localStorage.getItem('user'); 
+
+    if (!userString) {
+
+        console.log("Usuário não logado. Redirecionando...");
+
+        window.location.href = LOGIN_PAGE_URL; // Redirecionar
+
+        return null;
+
+    }
+    
+    try {
+
+        return JSON.parse(userString);
+
+    } catch (e) {
+
+        localStorage.removeItem('user'); // Deslogar
+
+        window.location.href = LOGIN_PAGE_URL; // Redirecionar
+
+        return null;
+
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    user = getUser();
+
+});
