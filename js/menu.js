3ad8a3173bdffda93f3a081849f947a0b4192491
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-09-28 00:26:32 -0300

    Username in menu

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,21 @@
 function toggleMenu(id) {
+
     const menu = document.getElementById(id);
 
     menu.classList.toggle('hide');
+
 }
+
+function displayUsername() {
+    
+    const userString = localStorage.getItem('user');
+    const nameElement = document.getElementById('username-display');
+    const user = JSON.parse(userString);
+
+    nameElement.textContent = user.username;
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(displayUsername, 50); 
+});
