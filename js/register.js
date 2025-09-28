4230f38cd6241da29b383f3f6db3598be3e89972
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-09-28 15:53:20 -0300

    Fix user register

--- js/register.js
+++ js/register.js
@@ -1,4 +1,4 @@
-document.getElementById('login-form').addEventListener('submit', async (e) => {
+document.getElementById('register-form').addEventListener('submit', async (e) => {
     e.preventDefault();
     
     const username = document.getElementById('username').value;
@@ -8,42 +8,25 @@ document.getElementById('login-form').addEventListener('submit', async (e) => {
     const location = document.getElementById('location').value;
     const description = document.getElementById('description').value;
 
-    try {
+    const response = await fetch(API_USERS_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(
+            {
+                username,
+                email,
+                password,
+                phone,
+                location,
+                description
+            }
+        )
+    });
 
-        const response = await fetch(API_USERS_URL, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(
-                {
-                    username,
-                    email,
-                    password,
-                    phone,
-                    location,
-                    description
-                }
-            )
-        });
+    const user = await response.json();
 
-        const user = await response.json();
-
-        if (response.ok) {
-            
-            localStorage.setItem('user', JSON.stringify(user)); 
-            
-            window.location.href = 'index.html';
-
-        } else {
-            
-            alert(`Erro no registro: ${user.message || 'Dados inválidos ou já registrados.'}`);
-
-        }
-
-    } catch (error) {
-
-        console.error('Erro na requisição de registro:', error);
-        alert('Ocorreu um erro na comunicação com o servidor.');
-
-    }
+    localStorage.setItem('user', JSON.stringify(user)); 
+    
+    window.location.href = 'index.html';
 
 });
