c3389d129535c8b6946a6eca55fdbf7329f1cca0
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-09-29 02:09:52 -0300

    Messages counter

--- js/menu.js
+++ js/menu.js
@@ -7,6 +7,44 @@ function toggleMenu(id) {
 
 }
 
+// CHAT:
+async function updateUnreadCount() {
+
+    const counterElement = document.getElementById('counter-mensages');
+    const chatRedElement = document.getElementById('chat-red');
+    const chatBlueElement = document.getElementById('chat-blue');
+    const userString = localStorage.getItem(LOCAL_STORAGE_KEY);
+    const user = JSON.parse(userString);
+    const currentUserId = user.id;
+    const response = await fetch(API_MESSAGES_URL, { method: 'GET' });
+    const allMessages = await response.json();
+
+    const unreadCount = allMessages.filter(message => 
+        message.receiver_id === currentUserId && message.read === false
+    ).length;
+
+    if (unreadCount > 0) {
+
+        counterElement.textContent = unreadCount;
+
+        chatRedElement.classList.remove('hide');
+        chatBlueElement.classList.add('hide');
+        
+    } else {
+
+        counterElement.textContent = '';
+
+        chatRedElement.classList.add('hide');
+        chatBlueElement.classList.remove('hide');
+        
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(updateUnreadCount, 50);
+});
+
 // MOSTRAR NOME DE USUARIO:
 function displayUsername() {
     
